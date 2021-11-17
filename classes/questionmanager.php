1381f7886eae5956fea5056f2e47dccb6451a23c
Adam R. Welle <arwelle@cert.org>
2021-11-17 13:35:58 -0500

    updates number format on point assignment form

--- classes/questionmanager.php
+++ classes/questionmanager.php
@@ -226,7 +226,7 @@ class questionmanager {
 
         } else {
             // display the form
-            $mform->set_data(array('points' => $rtqquestion->points));
+            $mform->set_data(array('points' => number_format($rtqquestion->points, 2)));
             $this->renderer->print_header();
             $this->renderer->addquestionform($mform);
             $this->renderer->footer();
